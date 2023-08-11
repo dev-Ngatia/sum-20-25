c038e2757eb4b8f5fb0797f8f908e3a9ced69a84
dev-Ngatia <64679798+dev-Ngatia@users.noreply.github.com>
2023-08-11 11:42:06 +0300

    Create sum-20-25.java

---
+++ sum-20-25.java
@@ -0,0 +1,11 @@
+public class SumNumbers {
+    public static void main(String[] args) {
+        int sum = 0;
+
+        for (int i = 20; i <= 25; i++) {
+            sum += i;
+        }
+
+        System.out.println("Sum of numbers between 20 and 25: " + sum);
+    }
+}
